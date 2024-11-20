a1f51f8ec7458a1c6326b84e92f9fc8fdd09850d
rwaeng <rwaeng0421@gmail.com>
2024-11-20 20:32:07 +0900

    feat: 수정 페이지 생성 및 서버 연결

---
+++ app/edit/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { postCollection } from "@/utils/database";
+import { ObjectId } from "mongodb";
+
+export default async function Edit({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const result = await postCollection.findOne({ _id: new ObjectId(id) });
+
+  return (
+    <div className="p-20">
+      <h4>수정페이지</h4>
+      <form action="/api/post/edit" method="POST">
+        <input
+          name="title"
+          placeholder="글 제목"
+          defaultValue={result?.title}
+        />
+        <input
+          name="content"
+          placeholder="글 내용"
+          defaultValue={result?.content}
+        />
+        <input
+          style={{ display: "none" }}
+          name="_id"
+          defaultValue={result?._id.toString()}
+        />
+        <button type="submit">버튼</button>
+      </form>
+    </div>
+  );
+}
